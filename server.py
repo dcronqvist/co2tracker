55818f344a67735bd118540629cce9de2b92e6f5
Daniel Cronqvist <daniel@dcronqvist.se>
2020-11-19 12:08:40 +0100

    created base file-structure and updated readme

---
+++ server.py
@@ -0,0 +1,5 @@
+import flaskr
+
+# All this server.py file does, is import the the flask API module flaskr, and run the API.
+# This is mostly for making it easy to run when developing the API.
+flaskr.app.run(debug=True)
