e9e38752acbc197a9fa9abf19f3f64c0d6ef8217
Daniel Cronqvist <dancro99@gmail.com>
2020-12-04 13:25:20 +0100

    Changed db file to instead have the different collections

--- flaskr/db.py
+++ flaskr/db.py
@@ -3,5 +3,7 @@ import config
 
 client = pymongo.MongoClient(config.get_setting("mongodb-connection-string"),ssl=True,ssl_cert_reqs="CERT_NONE")
 db = client.testing
-collection = db.test
+coll_products = db.test
+coll_benchmarks = db.bench
+coll_transports = db.trans
 
