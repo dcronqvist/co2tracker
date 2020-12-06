089e2351d6936dcf93ddcddb9f14e539e90741da
Daniel Cronqvist <dancro99@gmail.com>
2020-12-06 13:56:55 +0100

    Added endpoint for getting all parents of a product

--- flaskr/benchmarks.py
+++ flaskr/benchmarks.py
@@ -76,3 +76,38 @@ def benchmarks_date():
     else:
         # If the payload doesn't pass payload check, return 400
         return make_response(jsonify(msg), 400)
+
+# SEARCH BENCHMARKS FOR PARENTS OF PRODUCT
+b_parents_sample = {
+    "product": ["str"],
+    "search": ["str"] # must be either "latest" or "all".
+}
+"""
+CURL-FRIENDLY TEST:
+$ curl -d '{"product": "accumulator", "search": "latest"}' -H "Content-Type: application/json" -X POST http://localhost:5000/benchmarks/get/parents
+"""
+@app.route("/benchmarks/get/parents", methods=["POST"])
+def benchmarks_parents():
+    query = request.get_json() # Get query as json
+    check, msg = check_payload(b_parents_sample, query) # Check payload
+
+    if check:
+        if query["search"] == "latest":
+            found = coll_benchmarks.find({ "sub_products.product": query["product"], "latest_benchmark": True }) # find all latest parent benchmarks for this product
+            l = list(found)
+            if len(l) > 0: # If there was at least one benchmark found.
+                return make_response(jsonify(l), 200)
+            else: # if there were no found benchmarks for that product on that date, then return "No benchmarks found."
+                return make_response(jsonify("No benchmarks found."), 404)
+        elif query["search"] == "all":
+            found = coll_benchmarks.find({ "sub_products.product": query["product"] }) # find all parent benchmarks for this product.
+            l = list(found)
+            if len(l) > 0: # If there was at least one benchmark found.
+                return make_response(jsonify(l), 200)
+            else: # if there were no found benchmarks for that product on that date, then return "No benchmarks found."
+                return make_response(jsonify("No benchmarks found."), 404)
+        else: # in case the request contains a strange value in key 'search'.
+            return make_response(jsonify(f"Unexpected value '{query['search']}' in key 'search'. Should be either 'latest' or 'all'."), 400)
+    else:
+        # If the payload doesn't pass payload check, return 400
+        return make_response(jsonify(msg), 400)
