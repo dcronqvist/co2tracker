499533bc1bbb35e7ef6eec1b3ff7e4f1d6035620
Daniel Cronqvist <dancro99@gmail.com>
2020-12-06 13:34:31 +0100

    Added endpoint for getting latest benchmark for product

--- flaskr/benchmarks.py
+++ flaskr/benchmarks.py
@@ -28,3 +28,26 @@ def benchmarks_all():
     else:
         # If the payload doesn't pass payload check, return 400
         return make_response(jsonify(msg), 400)
+
+# SEARCH ALL BENCHMARKS FOR PRODUCT
+b_latest_sample = {
+    "product": ["str"]
+}
+"""
+CURL-FRIENDLY TEST:
+$ curl -d '{"product": "accumulator"}' -H "Content-Type: application/json" -X POST http://localhost:5000/benchmarks/get/latest
+"""
+@app.route("/benchmarks/get/latest", methods=["POST"])
+def benchmarks_latest():
+    query = request.get_json() # Get query as json
+    check, msg = check_payload(b_latest_sample, query) # Check payload
+
+    if check:
+        found = coll_benchmarks.find_one({'product': query["product"], 'latest_benchmark': True}) # find all benchmarks for this product
+        if found: # If there are more than 0 found benchmarks, return them
+            return make_response(jsonify(found), 200)
+        else: # if there were no found benchmarks for that product, then return "No benchmarks found"
+            return make_response(jsonify("No benchmark found."), 404)
+    else:
+        # If the payload doesn't pass payload check, return 400
+        return make_response(jsonify(msg), 400)
