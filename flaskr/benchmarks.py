d74e2b5fa2257010ef862ff42e1b0d3b775fb9dc
Daniel Cronqvist <dancro99@gmail.com>
2020-12-10 20:50:22 +0100

    Added endpoint to create new benchmarks fix #11

--- flaskr/benchmarks.py
+++ flaskr/benchmarks.py
@@ -1,9 +1,11 @@
 import flask
 from flask import request, jsonify, make_response
 from flaskr import app, check_payload
-from flaskr.db import coll_benchmarks
+from flaskr.db import coll_benchmarks, coll_products
+from flaskr.bench_util import create_chain_impact, add_benchmark
 import datetime
 import json
+from datetime import datetime as dt
 
 # SEARCH ALL BENCHMARKS FOR PRODUCT
 b_all_sample = {
@@ -112,6 +114,56 @@ def benchmarks_parents():
         else:  # in case the request contains a strange value in key 'search'.
             s = f"Unexpected value '{query['search']}' in key 'search'. Should be either 'latest' or 'all'."
             return make_response(jsonify(s), 400)
+    else:
+        # If the payload doesn't pass payload check, return 400
+        return make_response(jsonify(msg), 400)
+
+
+# SEARCH BENCHMARKS FOR PRODUCT BY DATE
+b_create_sample = {
+    "product": ["str"],
+    "kg_per_unit": ["int", "float"],
+    "unit": ["str"],
+    "self_impact": {
+        "co2": ["int", "float"],
+        "measurement_error": ["float"],
+        "energy_sources": ["list:str"]
+    },
+    "sub_products": [
+        {
+            "product": ["str"],
+            "unit_amount": ["int", "float"],
+            "transport": ["int", "str"]
+        }
+    ]
+}
+"""
+CURL-FRIENDLY TEST:
+$ curl -d '{"product": "accumulator", "kg_per_unit": 2, "unit": "piece", "self_impact": { "co2": 0.15, "measurement_error": 0.02, "energy_sources": ["biogas"] }, "sub_products": [ { "product": "chs-märken", "unit_amount": 25, "transport": 69 }, { "product": "locomotive", "unit_amount": 1, "transport": 42 } ] }' -H "Content-Type: application/json" -X POST http://localhost:5000/benchmarks/create
+"""
+@app.route("/benchmarks/create", methods=["POST"])
+def benchmarks_create():
+    query = request.get_json()  # Get query as json
+    check, msg = check_payload(b_create_sample, query)  # Check payload
+
+    if check:
+        # Make sure that the product you're making a new benchmark for actually exists.
+        if not coll_products.find_one({"_id": query["product"]}):
+            return make_response(jsonify(f"ERROR: No product with ID '{query['product']}' exists."), 404)
+
+        benchmark = {
+            "product": query["product"],
+            "kg_per_unit": query["kg_per_unit"],
+            "unit": query["unit"],
+            "self_impact": {
+                "co2": query["self_impact"]["co2"],
+                "measurement_error": query["self_impact"]["measurement_error"],
+                "energy_sources": [es.lower() for es in query["self_impact"]["energy_sources"]]
+            },
+            "sub_products": query["sub_products"],
+        }
+        b = add_benchmark(benchmark)
+        return make_response(jsonify(b), 200)
     else:
         # If the payload doesn't pass payload check, return 400
         return make_response(jsonify(msg), 400)
